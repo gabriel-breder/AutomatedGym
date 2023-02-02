e7ccdc4710a29e159c57288f1816f718c94c96d0
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-02 15:11:08 -0300

    feat: loading fonts into the app

--- App.js
+++ App.js
@@ -2,20 +2,21 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 import { Routes } from './src/routes/Routes';
-import { GlobalProvider } from './src/context/GlobalContext';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <GlobalProvider>
-      <ThemeProvider theme={theme}>
-        <StatusBar
-          animated={true}
-          backgroundColor="transparent"
-          barStyle={"light-content"}
-          translucent={true}
-        />
-        <Routes />
-      </ThemeProvider>
-    </GlobalProvider>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        animated={true}
+        backgroundColor="transparent"
+        barStyle={"light-content"}
+        translucent={true}
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </ThemeProvider>
   );
 }
