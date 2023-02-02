05512cada497f21511573cf9657fbcd8a0d98a0b
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-01 21:52:33 -0300

    feat: add splash screen to routes

---
+++ src/routes/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from '../screens/Login';
+import Register from '../screens/Register';
+import Splash from '../screens/Splash';
+
+const Stack = createNativeStackNavigator();
+
+export const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
