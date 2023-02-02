a8967f45c840b2b8caaa820cef99b3e687917a87
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-01 21:38:53 -0300

    feat: splash screen v1

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Icon, BtnWrapper } from './styles';
 
-const Splash = () => {
+import Logo from '../../assets/images/logo.png';
+import { Button } from '../../components/Forms/Button';
+
+import BccImage from '../../assets/images/splash_background.jpg';
+
+const Splash = ({ navigation }) => {
   return (
-    <Container></Container>
+    <Container source={BccImage} resizeMode="cover">
+      <Icon
+        source={Logo}
+      />
+      <BtnWrapper>
+        <Button
+          title="ENTRAR"
+          onPress={() => navigation.navigate("Login")}
+        />
+        <Button
+          title="CRIAR CONTA"
+          onPress={() => navigation.navigate("Register")}
+        />
+      </BtnWrapper>
+    </Container>
   );
 }
 
--- src/screens/Splash/styles.js
+++ src/screens/Splash/styles.js
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.ImageBackground`
+  background-color: ${({ theme }) => theme.colors.background};
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 60px 0;
+`;
+
+export const Icon = styled.Image``;
+
+export const BtnWrapper = styled.View`
+  width: 80%;
+`;
