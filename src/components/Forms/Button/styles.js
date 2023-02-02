6052644fb5c83fd85dbabc73fc278ee90c2269de
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-01 21:49:31 -0300

    feat: updating button padding and background color

--- src/components/Forms/Button/styles.js
+++ src/components/Forms/Button/styles.js
@@ -4,14 +4,13 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { TouchableOpacity } from 'react-native';
 
 export const Container = styled(TouchableOpacity)`
-  background-color: #FCA311;
+  background-color: ${({ theme }) => theme.colors.orange};
+ 
   width: 100%;
   margin: 5px 0;
-  padding: 10px;
+  padding: 12px 10px;
 
   border-radius: 10px;
-
-  
 `;
 
 export const Title = styled.Text`
