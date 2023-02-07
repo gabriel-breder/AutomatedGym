157b31bffda1d7951fa7c394d3ce35aa9420fb39
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-07 16:37:36 -0300

    feat: add basic style and user context

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useContext } from 'react';
+import { Container, Header, Title, Separator } from './styles';
+
+import { FontAwesome } from '@expo/vector-icons';
+
+import { GlobalContext } from '../../context/GlobalContext';
 
 const Home = () => {
+  const context = useContext(GlobalContext);
+  const user = context.user;
+
   return (
     <Container>
-
+      <Header>
+        <Title>Olá, {user.name}!</Title>
+        <FontAwesome name="user-circle-o" size={40} color="white" />
+      </Header>
+      <Separator />
+      <Title>Próximo Treino</Title>
     </Container>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,5 +1,37 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
-  
+export const Container = styled.SafeAreaView`
+  background-color: ${({ theme }) => theme.colors.background};
+
+  flex: 1;
+  padding: ${getStatusBarHeight() + 20}px 20px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProfilePic = styled.Image`
+  background-color: #fff;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(20)}px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  width: 100%;
+
+  margin: 15px auto;
+
+  background-color: ${({ theme }) => theme.colors.shape};
+  opacity: .2;
 `;
