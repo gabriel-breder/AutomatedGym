c0e197485410d2c84aa5828bcc5b8f4ecd5c1e2d
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-06 22:22:31 -0300

    feat: add global provider into app

--- App.js
+++ App.js
@@ -4,19 +4,22 @@ import theme from './src/global/styles/theme';
 import { Routes } from './src/routes/Routes';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from './src/components/Loading';
+import { GlobalProvider } from './src/context/GlobalContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        animated={true}
-        backgroundColor="transparent"
-        barStyle={"light-content"}
-        translucent={true}
-      />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </ThemeProvider>
+    <GlobalProvider>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          animated={true}
+          backgroundColor="transparent"
+          barStyle={"light-content"}
+          translucent={true}
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </ThemeProvider>
+    </GlobalProvider>
   );
 }
