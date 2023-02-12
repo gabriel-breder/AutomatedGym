dfddab6fabb6101b34089613ffeb49e6d0ed4946
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-12 12:51:41 -0300

    feat: turn routes.js into mainStack and mainTab

--- src/routes/MainStack.js
+++ src/routes/MainStack.js
@@ -5,16 +5,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
 import Splash from '../screens/Splash';
+import { MainTab } from './MainTab';
 
 const Stack = createNativeStackNavigator();
 
-export const Routes = () => {
+export const MainStack = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer >
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={MainTab} />
       </Stack.Navigator>
     </NavigationContainer>
   );
