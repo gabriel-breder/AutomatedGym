d97cffc25e73cc8222ba0b2848b0f9db5d5bed46
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-12 12:52:04 -0300

    feat: change routes file

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
-import { Routes } from './src/routes/Routes';
+import { MainStack } from './src/routes/MainStack';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from './src/components/Loading';
 import { GlobalProvider } from './src/context/GlobalContext';
@@ -18,7 +18,7 @@ export default function App() {
           barStyle={"light-content"}
           translucent={true}
         />
-        {fontsLoaded ? <Routes /> : <Loading />}
+        {fontsLoaded ? <MainStack /> : <Loading />}
       </ThemeProvider>
     </GlobalProvider>
   );
