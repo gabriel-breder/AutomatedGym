94a5288c2c175ce9671a0cb2d99da0dc50452d61
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-13 17:18:13 -0300

    feat: add body screen

---
+++ src/screens/Body/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const Body = () => {
+  return (
+    <Container>
+      <Text style={{ color: 'white' }}>Meu Corpo</Text>
+    </Container>
+  );
+}
+
+export default Body;
