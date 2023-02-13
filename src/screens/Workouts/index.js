f86f2e7a3fc2e14f4c9240598c8ce3fc6630c4dc
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-13 17:18:31 -0300

    feat: add workout screen

---
+++ src/screens/Workouts/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const Workouts = () => {
+  return (
+    <Container>
+      <Text style={{ color: 'white' }}>Treinos</Text>
+    </Container>
+  );
+}
+
+export default Workouts;
