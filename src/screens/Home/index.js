b5168ed79267c5b44c796e33cdf7d4d8b265b94e
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-13 17:18:59 -0300

    feat: change user profile icon

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react';
 import { Container, Header, Title, Separator } from './styles';
 
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import { GlobalContext } from '../../context/GlobalContext';
+import { useTheme } from 'styled-components';
 
 const Home = () => {
   const context = useContext(GlobalContext);
   const user = context.user;
+  const theme = useTheme();
 
   return (
     <Container>
       <Header>
         <Title>Olá, {user.name}!</Title>
-        <FontAwesome name="user-circle-o" size={40} color="white" />
+        <FontAwesome5 name="user-circle" size={32} color={theme.colors.shape} />
       </Header>
       <Separator />
       <Title>Próximo Treino</Title>
