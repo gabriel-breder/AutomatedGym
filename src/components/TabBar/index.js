4fce2022c3964e5a0ac71de8295326a4f7b72288
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-15 22:57:50 -0300

    feat: add tab bar icons based on user role

--- src/components/TabBar/index.js
+++ src/components/TabBar/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Container, TabItem } from './styles';
 
-import { Octicons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Octicons, MaterialCommunityIcons, MaterialIcons, Ionicons } from '@expo/vector-icons';
 
 import theme from '../../global/styles/theme';
 
@@ -13,12 +11,34 @@ export const TabBar = ({ state, navigation }) => {
 
   const navigate = screen => navigation.navigate(screen);
 
+  const role = 'aluno';
+
   return (
     <Container>
-      <TabItem onPress={() => navigate('Home')}><MaterialCommunityIcons name="calendar-today" size={size} color={color(0)} /></TabItem>
-      <TabItem onPress={() => navigate('Activities')}><MaterialIcons name="history" size={size} color={color(1)} /></TabItem>
-      <TabItem onPress={() => navigate('Workouts')}><Octicons name="list-unordered" size={size} color={color(2)} /></TabItem>
-      <TabItem onPress={() => navigate('Body')}><MaterialCommunityIcons name="weight-lifter" size={size} color={color(3)} /></TabItem>
+      {/* Alunos */}
+      {
+        role === 'aluno' &&
+        <>
+          <TabItem onPress={() => navigate('Home')}><MaterialCommunityIcons name="calendar-today" size={size} color={color(0)} /></TabItem>
+          <TabItem onPress={() => navigate('Activities')}><MaterialIcons name="history" size={size} color={color(2)} /></TabItem>
+          <TabItem onPress={() => navigate('Workouts')}><Octicons name="list-unordered" size={size} color={color(3)} /></TabItem>
+          <TabItem onPress={() => navigate('Body')}><MaterialCommunityIcons name="weight-lifter" size={size} color={color(4)} /></TabItem>
+        </>
+      }
+
+      {/* Personal */}
+      {
+        role === 'personal' &&
+        <><TabItem onPress={() => navigate('Trainees')}><Ionicons name="people" size={size} color={color(1)} /></TabItem></>
+      }
+
+      {/* Admin */}
+      {
+        role === 'admin' &&
+        <>
+          <TabItem onPress={() => navigate('Users')}><Ionicons name="people" size={size} color={color(1)} /></TabItem>
+        </>
+      }
     </Container>
   );
 }
