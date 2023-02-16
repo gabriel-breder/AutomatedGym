e67d26750690fcef57e8d3cd2d0965edc63d6dc1
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-02-16 14:34:13 -0300

    feat: move files to admin directory

---
+++ src/screens/Admin/Users/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const Users = () => {
+  return (
+    <Container>
+      <Text style={{ color: 'white' }}>Alunos</Text>
+    </Container>
+  );
+}
+
+export default Users;
